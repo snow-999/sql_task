07ce8bf4675322de5c60781463e53016258190c8
khaled <khaledleader012@gmail.com>
2025-03-11 09:15:16 +0200

    fix select option in sql task

--- sql_task/src/main/java/org/example/Main.java
+++ sql_task/src/main/java/org/example/Main.java
@@ -2,12 +2,9 @@ package org.example;
 import java.sql.*;
 import java.util.Scanner;
 
-
 public class Main {
-    public static String insertIntoTable() {
+    public static String insertIntoTable(String tableName) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("enter table name");
-        String tableName = scan.nextLine();
         StringBuilder query = new StringBuilder();
         switch (tableName) {
             case "stores" -> {
@@ -24,7 +21,10 @@ public class Main {
 
                 System.out.println("enter item code");
                 int code = scan.nextInt();
-                query.append(String.format("INSERT INTO %s (itemCode, itemName) VALUES (%d, '%s')",tableName,code, name));
+
+                System.out.println("enter item StoreCode");
+                int StoreCode = scan.nextInt();
+                query.append(String.format("INSERT INTO %s (itemCode, itemName, storeCode) VALUES (%d, '%s', %d)",tableName,code, name, StoreCode));
             }
             default -> {
                 query.append("no table with that name");
@@ -33,21 +33,22 @@ public class Main {
         return query.toString();
     }
 
-    public static String selectFromTable() {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("enter table name");
-        String tableName = scan.nextLine();
+    public static String selectFromTable(String tableName) {
+        Scanner scanner = new Scanner(System.in);
         StringBuilder query = new StringBuilder();
         switch (tableName) {
-            case "stores" -> {
-                System.out.println("chose to select: storeCode, storeName or * for all");
-                String column = scan.nextLine();
-                query.append(String.format("SELECT %s FROM %s",column, tableName));
+            case "items" ->{
+                System.out.println("chose item code");
+                int code = scanner.nextInt();
+                query.append(String.format("select itemName from items join stores on stores.StoreCode = items.StoreCode where %s.itemCode = %d", tableName, code));
             }
-            case "items" -> {
-                System.out.println("chose to select: itemCode, itemName or * for all");
-                String column = scan.nextLine();
-                query.append(String.format("SELECT %s FROM %s",column, tableName));
+            case "stores" -> {
+                System.out.println("chose store code");
+                int code = scanner.nextInt();
+                query.append(String.format("select itemName from items join stores on stores.StoreCode = items.StoreCode where %s.storeCode = %d", tableName, code));
+            }
+            default -> {
+                System.out.println("no table selected");
             }
         }
         return query.toString();
@@ -63,23 +64,28 @@ public class Main {
         System.out.println("chose an operation{insert, select}");
         String operation = scan.nextLine();
 
+        System.out.println("enter table name");
+        String tableName = scan.nextLine();
+
         try {
             Connection connection = DriverManager.getConnection(url, username, password);
-
             Statement statement = connection.createStatement();
-            if (operation.equals("insert")) {
-                statement.executeUpdate(insertIntoTable());
-            }
-            else {
-                ResultSet resultSet = statement.executeQuery(selectFromTable());
-                while (resultSet.next()) {
-                    String storeName = resultSet.getString("storeName");
-                    int storeId = resultSet.getInt("storeCode");
-                    System.out.println(storeName + " " + storeId);
+
+            switch (operation) {
+                case "insert" -> {
+                    statement.executeUpdate(insertIntoTable(tableName));
                 }
-
+                case "select" -> {
+                    ResultSet resultSet = statement.executeQuery(selectFromTable(tableName));
+                    while (resultSet.next()) {
+                        String itemName = resultSet.getString("itemName");
+                        System.out.println(itemName);
+                    }
+                }
+                default -> {
+                    System.out.println("no operation selected");
+                }
             }
-
             statement.close();
             connection.close();
 
